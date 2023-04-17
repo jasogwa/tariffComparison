b3db991be7792fc63c7da72b1c9ac508cc87d7e4
Jonas Asogwa <jasogwa@nanocosmos.net>
2023-04-17 09:16:43 +0200

    (backend): tariff data

---
+++ back-end/src/data.ts
@@ -0,0 +1,18 @@
+const externalTariffProvider = [
+    {
+        name: 'Product A',
+        type: 1,
+        includedKwh: 0,
+        baseCost: 5,
+        additionalKwhCost: 22
+    },
+    {
+        name: 'Product B',
+        type: 2,
+        includedKwh: 4000,
+        baseCost: 800,
+        additionalKwhCost: 30
+    }
+];
+
+export default externalTariffProvider;
